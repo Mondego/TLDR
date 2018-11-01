40ae968c7a128abb55a92dc6d8133b4fe441939b
marufzaber <mzaber@uci.edu>
2018-11-01 12:46:51 -0700

    initialize the maven project

---
+++ src/main/java/uci/ics/mondego/tldr/App.java
@@ -0,0 +1,13 @@
+package uci.ics.mondego.tldr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
