514cd7c0dbe49e0773300d6e673374bd3a629d80
marufzaber <mzaber@uci.edu>
2018-11-12 16:42:07 -0800

    scanner added

---
+++ src/main/java/uci/ics/mondego/tldr/extractor/Scanner.java
@@ -0,0 +1,14 @@
+package uci.ics.mondego.tldr.extractor;
+
+public class Scanner {
+	
+	String PROJ_DIR;
+	List<Fil>
+	
+	
+	
+	public Scanner(){
+		PROJ_DIR = System.getProperty("user.dir");
+	}
+
+}
