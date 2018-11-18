4ed21c541544a1e6a29bd0fbb30e70c0964d7b81
marufzaber <mzaber@uci.edu>
2018-11-18 15:57:58 -0800

    scanner added

--- src/main/java/uci/ics/mondego/tldr/extractor/Scanner.java
+++ src/main/java/uci/ics/mondego/tldr/extractor/Scanner.java
@@ -1,6 +1,10 @@
 package uci.ics.mondego.tldr.extractor;
 
+import java.io.File;
 import java.util.List;
+import org.eclipse.core.*;
+import org.eclipse.jdt.core.dom.ASTParser;
+
 
 public class Scanner {
 	
@@ -8,9 +12,27 @@ public class Scanner {
 	
 	List<String> all_files;
 	
-	
 	public Scanner(){
 		PROJ_DIR = System.getProperty("user.dir");
 	}
+	
+	
+	/* gets all file from the project directory*/
+	
+	public void listf(String directoryName, List<File> files) {
+	    File directory = new File(directoryName);
 
-}
+	    File[] fList = directory.listFiles();
+	    
+	    if(fList != null)
+	        for (File file : fList) {      
+	            if (file.isFile()) {
+	                files.add(file);
+	            } else if (file.isDirectory()) {
+	                listf(file.getAbsolutePath(), files);
+	            }
+	        }
+	    }
+	}
+
+
