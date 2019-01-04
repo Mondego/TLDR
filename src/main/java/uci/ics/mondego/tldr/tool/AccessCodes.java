4e9895f2852a2fd1a4c99897c7849267b88af862
marufzaber <mzaber@uci.edu>
2019-01-03 16:57:54 -0800

    Dependent sync implemented, better logging support added, bug fixed for external dependency

---
+++ src/main/java/uci/ics/mondego/tldr/tool/AccessCodes.java
@@ -0,0 +1,29 @@
+package uci.ics.mondego.tldr.tool;
+
+public class AccessCodes {
+	
+	public static final int ABSTRACT = 1024;
+
+	public static final int FINAL = 16;
+
+	public static final int INTERFACE = 512;
+
+	public static final int NATIVE = 256;
+
+	public static final int PRIVATE = 2;
+
+	public static final int PROTECTED = 4;
+
+	public static final int PUBLIC = 1;
+
+	public static final int STATIC = 8;
+
+	public static final int STRICT = 2048;
+
+	public static final int SYNCHRONIZED = 32;
+
+	public static final int TRANSIENT = 128;
+
+	public static final int VOLATILE = 64;
+
+}
