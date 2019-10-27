6d5e6c8667a1967822161555ac2610a979d114a8
marufzaber <mzaber@uci.edu>
2019-10-27 15:00:08 -0700

    styx project directory corrected

--- script/experiment_change_analysis.sh
+++ script/experiment_change_analysis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 base_dir="/scratch/mondego/local/Maruf"
-sample_project_dir=$base_dir'/PROJECT'
+sample_project_dir=$base_dir'/PROJECTS'
 tldr_dir=$base_dir'/TLDR'
 
 for i in $(ls $sample_project_dir);
