d531fa1185e9210a41403b24a90a628cc2da9707
marufzaber <mzaber@uci.edu>
2019-10-30 12:32:49 -0700

    parallization added in the experiment script

--- script/experiment_change_analysis.sh
+++ script/experiment_change_analysis.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-base_dir="/scratch/mondego/local/Maruf"
+base_dir="/lv_scratch/scratch/mondego/local/Maruf"
 sample_project_dir=$base_dir'/PROJECTS'
 tldr_dir=$base_dir'/TLDR'
 
 for i in $(ls $sample_project_dir);
 do
 	test_project=$sample_project_dir/$i
-	./change_analysis.sh $tldr_dir $test_project $i 
+	./change_analysis.sh $tldr_dir $test_project $i &
 done
 
 
