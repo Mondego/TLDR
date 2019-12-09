3372122665ddb92ecf6faeea9b58d93bb44f137d
marufzaber <mzaber@uci.edu>
2019-12-09 15:44:21 -0800

    Parameters defined in Mojo classes

--- src/main/java/uci/ics/mondego/tldr/plugin/BaseMojo.java
+++ src/main/java/uci/ics/mondego/tldr/plugin/BaseMojo.java
@@ -1,7 +1,15 @@
 package uci.ics.mondego.tldr.plugin;
 
+import java.io.File;
+
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 public abstract class BaseMojo extends AbstractMojo {
 
+	@Parameter(defaultValue = "${project.build.directory}")
+    protected String projectBuildDir;
+
+    @Parameter(defaultValue = "${basedir}")
+    protected File basedir;
 }
--- src/main/java/uci/ics/mondego/tldr/plugin/ReportMojo.java
+++ src/main/java/uci/ics/mondego/tldr/plugin/ReportMojo.java
@@ -2,11 +2,14 @@ package uci.ics.mondego.tldr.plugin;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
 
+@Mojo( name = "report-tldr")
 public class ReportMojo extends AbstractMojo {
 	
-	public void execute() throws MojoExecutionException {
-		
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		getLog().debug("Printing Reports");
 	}
 
 }
